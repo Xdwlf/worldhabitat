26e61a8c1a88e1ff1852abc0a75ffc2a0e117e08
Rowan Lin <xdwlf32@gmail.com>
2018-12-26 18:40:43 -0800

    updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-var config= require("./config.js");
+var config= require("config");
 
 var creatures= {};
 
